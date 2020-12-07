5e9d1a199f3860055daa8df96bf5f262fce438b1
krishanoviks <krishanoviks@gmail.com>
2020-12-07 12:05:18 +0200

    ads

--- uzdevumi/taylorseries.c
+++ uzdevumi/taylorseries.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-long double approx = 1,x,a = 1.0;
-int k = 1;
+long double approx = 1,x,a = 1.0,pirmspedejais;
+int k;
 
 int main(){
 
@@ -12,9 +12,10 @@ int main(){
 
  while(k<1000)
  {
+   pirmspedejais = a;
+   k++;
    a = a* (long double) (-x)/k;
    approx += a;
-   k++;
  }
 
  // zimesana
@@ -25,13 +26,16 @@ int main(){
  printf("               /____      k!     \n");
  printf("               k = 0             \n");
 
- printf("                          (-x)\n");
+ printf("\n                          (-x)\n");
  printf("rekursijas reizinatajs = _____\n");
  printf("                           k  \n");
 
- printf("funkcija defineta visas realas x vertibas\n");
- printf("pec teilora rindas aprekinatais e^(-%.2Lf) = %.20LE\n",x,approx);
- printf("pec exp(-x) aprekinatais e^(-%.2Lf) = %.20LE\n",x,exp(-x));
+
+ printf("\npedejais loceklis = %Le\n",a);
+ printf("pirmspedejais loceklis = %Le\n",pirmspedejais);
+ printf("\nfunkcija defineta visas realas x vertibas\n");
+ printf("\npec teilora rindas aprekinatais e^(-%.2Lf) = %.50LE\n",x,approx);
+ printf("pec exp(-x) aprekinatais e^(-%.2Lf) = %.50LE\n",x,exp(-x));
 
 
 
